133aabcceefd5d95f0a44df10750b8fc1fb018b9
nestormarlier <47480842+nestormarlier@users.noreply.github.com>
2023-10-22 17:55:09 -0300

    clase eliminada en models.py

--- multiseleccion/models.py
+++ multiseleccion/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 from django import forms
 
-class TareasFilterForm(forms.Form):
-    ESTADO_TAREA = (
-        ('PENDIENTE', 'Pendiente'),
-        ('PROCESO', 'En proceso'),
-        ('COMPLETADA', 'Completada')
-    )
-    estado = forms.MultipleChoiceField(
-        choices=ESTADO_TAREA,
-        required=False,  # Permite que el campo sea opcional
-        widget=forms.CheckboxSelectMultiple  # Utiliza checkboxes para la selección múltiple
-    )
-
 class Tareas(models.Model):
     ESTADO_TAREA = (
         ('PENDIENTE', 'Pendiente'),
@@ -20,7 +8,7 @@ class Tareas(models.Model):
         ('COMPLETADA', 'Completada')
                     )
     nombre=models.CharField(max_length=50, blank=False)
-    state = models.CharField(max_length=10, choices=ESTADO_TAREA, blank=True)
+    state = models.CharField(max_length=10, choices=ESTADO_TAREA, blank=False)
 
     def __str__(self):
         return self.nombre
